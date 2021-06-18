12fcd99e21fe7521d727dc4c5b34fdc22c2f73cb
kphung29 <kphung29@gmail.com>
2021-06-18 11:13:54 -0500

    update

--- randomUserGenerator/app.js
+++ randomUserGenerator/app.js
@@ -7,6 +7,16 @@ const app = Vue.createApp({
       gender: 'male',
       picture: 'https://randomuser.me/api/portraits/men/75.jpg'
     }
+  },
+  methods: {
+    async getUser() {
+      const res = await fetch('https://randomuser.me/api')
+      const { results } = await res.json();
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.picture = results[0].picture.large;
+    }
   }
 })
 
